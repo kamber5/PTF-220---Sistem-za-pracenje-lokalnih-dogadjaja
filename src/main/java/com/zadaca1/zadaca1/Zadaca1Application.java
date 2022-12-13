f870b4e8e56b7b13348b390d5eb7936a1b875723
kamber5 <35629701+kamber5@users.noreply.github.com>
2022-12-13 01:24:05 +0100

    Gennerated start files, sql key added

---
+++ src/main/java/com/zadaca1/zadaca1/Zadaca1Application.java
@@ -0,0 +1,13 @@
+package com.zadaca1.zadaca1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zadaca1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Zadaca1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/zadaca1/zadaca1/Zadaca1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zadaca1.zadaca1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zadaca1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
